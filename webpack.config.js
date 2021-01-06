185df951e234d488ebe953556276e2cbd60b9b3c
Marina Osborne <marina08088@mail.ru>
2021-01-06 20:39:49 +0300

    new

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+   
+    {test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader']}
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+    template: "./src/index.html",
+    filename: "./index.html"
+    }),
+    
+    new MiniCssExtractPlugin({
+    filename: '[name].css'
+    })
+    ],
+    
+};
+
+
+
